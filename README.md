4d730f0c4944bd91ae8d50387aa278030dca952b
Property404 <git@dagans.dev>
2024-10-19 17:31:31 -0500

    Switch to clap

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # devserver [![Crates.io](https://img.shields.io/crates/v/devserver.svg)](https://crates.io/crates/devserver)
 
-
 An extremely tiny tool to serve a static folder locally.
 
 This tool is only for local development and makes no effort to be secure for other purposes.
@@ -12,26 +11,17 @@ devserver should only be used for locally hosting files on a trusted network.
 devserver does not properly handle the attacks robust servers must withstand on an open network.
 
 ## Installation
+
 ```
 cargo install devserver
 ```
 
 ## Usage
+
 Open a command line and navigate to the directory you'd like to host then run:
+
 ```
 devserver
 ```
 
 Visit http://localhost:8080 or https://localhost:8080 to see your hosted content.
-
-## Options
-`--reload`  Automatically refresh pages when a file in the hosted folder changes.
-
-`--address` Pass an address like "127.0.0.1:8080" or "localhost:8000" to change the address the server will host on.
-
-`--path`    Changes the directory to be hosted.
-
-`--header`  Pass a header like "Access-Control-Allow-Origin='\*'". Use multiple `--header` flags for multiple headers.
-
-`--help`    Explains available options.
-
--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,103 @@
+use anyhow::{bail, Result};
+use clap::Parser;
 use std::env;
 use std::fmt::Display;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use std::path::PathBuf;
 use std::process::Command;
 
-fn main() {
-    let mut address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-    let mut path = PathBuf::new();
-    let mut headers = "".to_string();
-    let mut reload = true;
+const DEFAULT_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    /// Build command to run when source files change
+    #[clap(short, long)]
+    build: Option<String>,
+    /// Don't automatically reload
+    #[clap(long)]
+    noreload: bool,
+    /// The address/port
+    #[clap(short, long, default_value_t=DEFAULT_ADDR)]
+    address: SocketAddr,
+    /// The path to serve
+    #[clap(long)]
+    path: Option<PathBuf>,
+    /// Extra headers to serve
+    #[clap(long)]
+    header: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let args = Arguments::parse();
     let mut actions = Vec::new();
+    if let Some(cmd) = args.build {
+        let func: devserver::Action = Box::new(move || {
+            let mut command = Command::new("sh");
+            let command = command.arg("-c").arg(&cmd);
+            let output = command.output().map_err(|err| {
+                let err: Box<dyn Display> = Box::new(err);
+                err
+            })?;
+            let stdout = String::from_utf8(output.stdout).unwrap();
+            print!("{stdout}");
+            let stderr = String::from_utf8(output.stderr).unwrap();
+            print!("{stderr}");
+            if !output.status.success() {
+                return Err(Box::new(format!("{stdout}{stderr}")));
+            }
+            Ok(())
+        });
+        actions.push(func);
+    };
 
-    let mut args = env::args().skip(1);
-    while let Some(arg) = args.next() {
-        match arg.as_ref() {
-            "--address" => {
-                address = args
-                    .next()
-                    .expect("Pass an address with a port after the '--address' flag")
-                    .to_socket_addrs()
-                    .expect("Please give address and port in form of '127.0.0.1:8080'")
-                    .next()
-                    .unwrap()
-            }
-            "-b" | "--build" => {
-                let cmd = args.next().expect("--build expected argument");
-                let func: devserver::Action = Box::new(move || {
-                    let mut command = Command::new("sh");
-                    let command = command.arg("-c").arg(&cmd);
-                    let output = command.output().map_err(|err| {
-                        let err: Box<dyn Display> = Box::new(err);
-                        err
-                    })?;
-                    let stdout = String::from_utf8(output.stdout).unwrap();
-                    print!("{stdout}");
-                    let stderr = String::from_utf8(output.stderr).unwrap();
-                    print!("{stderr}");
-                    if !output.status.success() {
-                        return Err(Box::new(format!("{stdout}{stderr}")));
-                    }
-                    Ok(())
-                });
-                actions.push(func);
-            }
-            "--reload" | "--refresh" => reload = true,
-            "--noreload" | "--norefresh" => reload = false,
-            "--path" => {
-                path = args
-                    .next()
-                    .expect("Pass a path after the '--path' flag")
-                    .into()
-            }
-            "--header" => {
-                let mut new_header = args
-                    .next()
-                    .expect("Pass a header after the '--header' flag");
-                if !new_header.contains(':') {
-                    if new_header.contains('=') {
-                        new_header = new_header.replacen('=', ":", 1);
-                    } else {
-                        panic!("Pass a ':' or '=' in the '--header' flag");
-                    }
-                }
-                if new_header.contains('\r') || new_header.contains('\n') || !new_header.is_ascii()
-                {
-                    panic!("Only ASCII without line breaks is allowed in the '--header' flag");
-                }
-                headers.push_str("\r\n");
-                headers.push_str(&new_header);
-            }
-            "--help" => {
-                println!(
-                    r#"Run 'devserver' in a folder to host that folder.
-
---reload                   Automatically refresh pages when a file in the hosted folder changes. Enabled by default.
---noreload                 Do not automatically refresh pages when a file in the hosted folder changes.
---address [address]:[port] Specify an address to use. The default is 'localhost:8080'.
---path [path]              Specify the path of the folder to be hosted.
---header                   Specify an additional header to send in responses. Use multiple --header flags for multiple headers.
---help                     Display the helpful information you're reading right now.
-
-Examples:
-
-devserver --address 127.0.0.1:8080 --path "some_directory/subdirectory" --header Access-Control-Allow-Origin='*'
-
-                "#
-                );
-                return;
-            }
-            _ => {
-                println!(
-                    "Unrecognized flag: `{:?}`.\nSee available options with `devserver --help`",
-                    arg
-                );
-                return;
-            }
+    let mut headers = String::new();
+    for header in args.header.iter().cloned().map(|h| {
+        if !h.contains(':') {
+            bail!("Header must contain ':'");
         }
+        if h.contains('\r') || h.contains('\n') {
+            bail!("Header cannot contain line breaks");
+        }
+        if !h.is_ascii() {
+            bail!("Header must be ASCII");
+        }
+        Ok(h)
+    }) {
+        headers.push_str("\r\n");
+        headers.push_str(&header?);
     }
-    let hosted_path = env::current_dir().unwrap().join(path);
+
+    let hosted_path = env::current_dir()
+        .unwrap()
+        .join(args.path.unwrap_or_default());
 
     if !hosted_path.exists() {
-        println!("Path [{}] does not exist!", hosted_path.display());
-        return;
+        bail!("Path [{}] does not exist!", hosted_path.display());
     }
 
     println!(
         "\nServing [{}] at [ https://{} ] or [ http://{} ]",
         hosted_path.display(),
-        address.ip(),
-        address.port()
+        args.address.ip(),
+        args.address.port()
     );
 
-    if reload {
+    if !args.noreload {
         println!("Automatic reloading is enabled!");
     }
 
     println!("Stop with Ctrl+C");
 
     devserver::run(
-        address.ip(),
-        address.port(),
+        args.address.ip(),
+        args.address.port(),
         &hosted_path,
-        reload,
+        !args.noreload,
         &headers,
         actions,
     );
+
+    Ok(())
 }
